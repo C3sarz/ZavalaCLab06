4536dc9980efe8bf6012550d161c84b24d9257bc
Cesar Zavala <cesarz@ksu.edu>
2019-10-15 17:23:53 -0500

    .

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list {
+    int item;
+    struct list *next;
+}List;
+
+void nodeDelete(List **nodePtrtoPtr, int data){
+
+    while(nodePtrtoPtr != NULL){
+        if((*nodePtrtoPtr)->next != NULL && (*nodePtrtoPtr)->next->item == data){
+            if((*nodePtrtoPtr)->next->next == NULL){
+                (*nodePtrtoPtr)->next = NULL;
+            }
+            else (*nodePtrtoPtr)->next = (*nodePtrtoPtr)->next->next;
+            return;
+        }
+        else {*nodePtrtoPtr = (*nodePtrtoPtr)->next;}
+    }
+}
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    List *node3 = malloc(sizeof(List));
+    node3->item = 3;
+    node3->next = NULL;
+
+    List *node2 = malloc(sizeof(List));
+    node2->item = 2;
+    node2->next = node3;
+
+    List *node1 = malloc(sizeof(List));
+    node1->item = 1;
+    node1->next = node2;
+
+    int i = 2;
+
+    nodeDelete(&node1,i);
+
+    List * current = node1;
+    while(current != NULL){
+        printf("%d\n",current->item);
+        current = current->next;
+    }
+return 0;
 }
